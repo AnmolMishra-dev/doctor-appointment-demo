ebe07d766f7d3fec8dcd9d87003e26f38c91a212
Anmol Mishra <tbsdev3@gmail.com>
2022-06-09 11:39:31 +0530

    demo

---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+// @dart=2.9
+import 'package:flutter/material.dart';
+
+import 'Appointment_Page.dart';
+import 'Come_back_page.dart';
+import 'Home_page.dart';
+import 'Schedule_Page.dart';
+import 'Sign_up_Page.dart';
+import 'create_account_Page.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:workmanager/workmanager.dart';
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Workmanager.initialize(
+
+    // The top level function, aka callbackDispatcher
+      callbackDispatcher,
+
+      // If enabled it will post a notification whenever
+      // the task is running. Handy for debugging tasks
+      isInDebugMode: true
+  );
+  // Periodic task registration
+  Workmanager.registerPeriodicTask(
+    "2",
+
+    //This is the value that will be
+    // returned in the callbackDispatcher
+    "simplePeriodicTask",
+
+    // When no frequency is provided
+    // the default 15 minutes is set.
+    // Minimum frequency is 15 min.
+    // Android will automatically change
+    // your frequency to 15 min
+    // if you have configured a lower frequency.
+    frequency: Duration(minutes: 15),
+  );
+  runApp(MyApp());
+}
+void callbackDispatcher() {
+  Workmanager.executeTask((task, inputData) {
+
+    // initialise the plugin of flutterlocalnotifications.
+    FlutterLocalNotificationsPlugin flip = new FlutterLocalNotificationsPlugin();
+
+    // app_icon needs to be a added as a drawable
+    // resource to the Android head project.
+    var android = new AndroidInitializationSettings('@mipmap/ic_launcher');
+    var IOS = new IOSInitializationSettings();
+
+    // initialise settings for both Android and iOS device.
+    var settings = new InitializationSettings(android: android, iOS: IOS);
+    flip.initialize(settings);
+    _showNotificationWithDefaultSound(flip);
+    return Future.value(true);
+  });
+}
+
+Future _showNotificationWithDefaultSound(flip) async {
+
+  // Show a notification after every 1 minute with the first
+  // appearance happening a minute after invoking the method
+  var androidPlatformChannelSpecifics = new AndroidNotificationDetails(
+      'your channel id',
+      'your channel name',
+      'your channel description',
+      importance: Importance.max,
+      priority: Priority.high
+  );
+  var iOSPlatformChannelSpecifics = new IOSNotificationDetails();
+
+  // initialise channel platform for both Android and iOS device.
+  var platformChannelSpecifics = new NotificationDetails(
+      android: androidPlatformChannelSpecifics,
+      iOS: iOSPlatformChannelSpecifics
+  );
+  await flip.show(0, 'Doctor Appointment App',
+      'Your are one step away to connect with Doctor Appointment App',
+      platformChannelSpecifics, payload: 'Default_Sound'
+  );
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(
+
+      ),
+    );
+  }
+}
+
